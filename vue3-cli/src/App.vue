dd81168cf8c960ea130993fd36cd8dd69904eb42
BrandonJE <khaos1004@naver.com>
2023-12-14 15:59:29 +0900

    first-commit

---
+++ vue3-cli/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <AppHeader @change="changeMessage"  v-bind:appTitle="message"></AppHeader>  
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue';
+
+  export default {
+    components:{
+      // '컴포넌트이름': 컴포넌트내용
+      // 'app-header': AppHeader 아래와같이 키값도 파스칼케이스로 작성
+      // 'AppHeader': AppHeader 아래와같이 키와 밸류가 같을경우 아래와같이 한단어로 표현 가능
+      // AppHeader
+      AppHeader
+    },
+    data() {
+      return {
+        message: '앱 해더 컴포넌트'
+      }
+    },
+    methods:{
+      changeMessage(){
+        this.message = '변경됨'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue3-cli/src/components/AppHeader.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>{{ appTitle }}</h1>
+        <button class="animate-spin" @click="changeTitle">체인지</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        props:['appTitle'],
+        methods:{
+            changeTitle(){
+                this.$emit('change');
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
